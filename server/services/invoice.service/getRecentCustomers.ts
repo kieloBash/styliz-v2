3bb08952e617ab6134b13ec766e2115e7627cf30
user <114970278+kieloBash@users.noreply.github.com>
2025-08-25 00:36:22 +0800

    fix: endpoint for customer analytics

--- server/services/invoice.service/getRecentCustomers.ts
+++ server/services/invoice.service/getRecentCustomers.ts
@@ -3,12 +3,15 @@ import { protectedProcedure } from "@/server/trpc/init";
 import { QueryPayloadType } from "@/types/global";
 import { logger } from "@/utils/logger";
 import { fetchRecentCustomers } from "./queries";
+import { startOfMonth, endOfMonth } from "date-fns";
 
 export const getRecentCustomers = protectedProcedure
     .input(SearchCustomerSchema)
     .query(async ({ input, ctx }): Promise<QueryPayloadType<any[]>> => {
         try {
-            const customers = await fetchRecentCustomers(ctx.db!, input.limit ?? 5);
+            const from = input.from ?? startOfMonth(new Date()).toISOString()
+            const to = input.to ?? endOfMonth(new Date()).toISOString()
+            const customers = await fetchRecentCustomers(ctx.db!, input.limit ?? 5, from, to);
             return { success: true, message: "Successfully fetched recent customers", payload: customers };
         } catch (error) {
             const message = "failed to fetch recent customers";
--- server/services/invoice.service/getTopCustomers.ts
+++ server/services/invoice.service/getTopCustomers.ts
@@ -3,12 +3,15 @@ import { protectedProcedure } from "@/server/trpc/init";
 import { QueryPayloadType } from "@/types/global";
 import { logger } from "@/utils/logger";
 import { fetchTopCustomers } from "./queries";
+import { startOfMonth, endOfMonth } from "date-fns";
 
 export const getTopCustomers = protectedProcedure
     .input(SearchCustomerSchema)
     .query(async ({ input, ctx }): Promise<QueryPayloadType<any[]>> => {
         try {
-            const customers = await fetchTopCustomers(ctx.db!, input.limit ?? 5);
+            const from = input.from ?? startOfMonth(new Date()).toISOString()
+            const to = input.to ?? endOfMonth(new Date()).toISOString()
+            const customers = await fetchTopCustomers(ctx.db!, input.limit ?? 5, from, to);
             return { success: true, message: "Successfully fetched top spending customers", payload: customers };
         } catch (error) {
             const message = "failed to fetch top customers";
