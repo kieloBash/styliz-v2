724afba8a7f8d19c29f6998364be520d3f290048
user <114970278+kieloBash@users.noreply.github.com>
2025-09-14 16:50:40 +0800

    chore: more data for seller

--- constants/formats.ts
+++ constants/formats.ts
@@ -1,2 +1,2 @@
 export const DATE_FORMAT_SHORT = "MM/dd/yyyy";
-export const DEFAULT_MINIMUM_RATE = 400;
+export const DEFAULT_MINIMUM_RATE = 450;
---
+++ constants/seller-data.ts
@@ -0,0 +1,29 @@
+import { DEFAULT_MINIMUM_RATE } from "./formats";
+
+export const SELLERS = [
+    {
+        name: "Liz",
+        email: "liz@gmail.com",
+        rate: DEFAULT_MINIMUM_RATE,
+    },
+    {
+        name: "Phaloma",
+        email: "phaloma@gmail.com",
+        rate: 550,
+    },
+    {
+        name: "zia",
+        email: "zia@gmail.com",
+        rate: 550,
+    },
+    {
+        name: "Jai",
+        email: "jai@gmail.com",
+        rate: 500,
+    },
+    {
+        name: "Shein",
+        email: "shein@gmail.com",
+        rate: 450,
+    },
+];
