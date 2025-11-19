a85df03eb6e8c101c1bec7f74b15c236afcea340
user <114970278+kieloBash@users.noreply.github.com>
2025-11-19 22:55:04 +0800

    fix: updates on middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,42 +1,40 @@
-import { NextResponse } from "next/server";
 import { auth } from "./auth";
-import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes } from "./routes";
 
 
 export default auth(async (req: any) => {
-    const { nextUrl } = req;
-    const isLoggedIn = !!req.auth;
+    // const { nextUrl } = req;
+    // const isLoggedIn = !!req.auth;
 
-    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-    const isPublicRoute = publicRoutes.some((route) => {
-        return route === "/"
-            ? nextUrl.pathname === route
-            : nextUrl.pathname.startsWith(route);
-    });
-    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+    // const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    // const isPublicRoute = publicRoutes.some((route) => {
+    //     return route === "/"
+    //         ? nextUrl.pathname === route
+    //         : nextUrl.pathname.startsWith(route);
+    // });
+    // const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-    if (isApiAuthRoute || isPublicRoute) return;
+    // if (isApiAuthRoute || isPublicRoute) return;
 
-    if (isAuthRoute) {
-        if (isLoggedIn) {
-            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
-        }
-        return;
-    }
+    // if (isAuthRoute) {
+    //     if (isLoggedIn) {
+    //         return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    //     }
+    //     return;
+    // }
 
-    if (!isLoggedIn && !isPublicRoute) {
-        // let callbackUrl = nextUrl.pathname;
-        // if (nextUrl.search) {
-        //     callbackUrl += nextUrl.search;
-        // }
-        // const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+    // if (!isLoggedIn && !isPublicRoute) {
+    //     // let callbackUrl = nextUrl.pathname;
+    //     // if (nextUrl.search) {
+    //     //     callbackUrl += nextUrl.search;
+    //     // }
+    //     // const encodedCallbackUrl = encodeURIComponent(callbackUrl);
 
-        const url = new URL("/auth/sign-in", req.url);
-        // url.searchParams.set("reason", "SignedOut");
-        // url.searchParams.set("callbackUrl", encodedCallbackUrl);
+    //     const url = new URL("/auth/sign-in", req.url);
+    //     // url.searchParams.set("reason", "SignedOut");
+    //     // url.searchParams.set("callbackUrl", encodedCallbackUrl);
 
-        return NextResponse.redirect(url);
-    }
+    //     return NextResponse.redirect(url);
+    // }
 
     return;
 });
